7e09543d81838cb13006b71d8af7108880d11b31
hodhanhassan99 <ajfreeforall@gmail.com>
2026-06-13 20:56:01 +0300

    added changes

--- _config.js
+++ _config.js
@@ -1,9 +1,9 @@
 var config = {}
 
 config.mongoURI = {
-    production: 'mongodb://hodhan:6161332200p@ac-niamzvk-shard-00-00.v9ky2b5.mongodb.net:27017,ac-niamzvk-shard-00-01.v9ky2b5.mongodb.net:27017,ac-niamzvk-shard-00-02.v9ky2b5.mongodb.net:27017/darkroom?ssl=true&replicaSet=atlas-t67mzw-shard-0&authSource=admin&retryWrites=true&w=majority',
-    development: 'mongodb://hodhan:6161332200p@ac-niamzvk-shard-00-00.v9ky2b5.mongodb.net:27017,ac-niamzvk-shard-00-01.v9ky2b5.mongodb.net:27017,ac-niamzvk-shard-00-02.v9ky2b5.mongodb.net:27017/darkroom-dev?ssl=true&replicaSet=atlas-t67mzw-shard-0&authSource=admin&retryWrites=true&w=majority',
-    test: 'mongodb://hodhan:6161332200p@ac-niamzvk-shard-00-00.v9ky2b5.mongodb.net:27017,ac-niamzvk-shard-00-01.v9ky2b5.mongodb.net:27017,ac-niamzvk-shard-00-02.v9ky2b5.mongodb.net:27017/darkroom-test?ssl=true&replicaSet=atlas-t67mzw-shard-0&authSource=admin&retryWrites=true&w=majority'
+    production: 'mongodb+srv://hodhan:6161332200p@cluster0.v9ky2b5.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://hodhan:6161332200p@cluster0.v9ky2b5.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://hodhan:6161332200p@cluster0.v9ky2b5.mongodb.net/darkroom-test?retryWrites=true&w=majority'
 }
 
 module.exports = config;
