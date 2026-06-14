f7d043c1568f19c9dcd8197e40f6d6953205a159
hodhanhassan99 <ajfreeforall@gmail.com>
2026-06-14 18:47:59 +0300

    changed

--- _config.js
+++ _config.js
@@ -6,4 +6,4 @@ config.mongoURI = {
     test: 'mongodb+srv://hodhan:6161332200p@cluster0.v9ky2b5.mongodb.net/darkroom-test?retryWrites=true&w=majority'
 }
 
-module.exports = config;
+module.exports =  config;
