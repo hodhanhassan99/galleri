be82753336e8c3d7f48750b0545f51e8c87c6fad
hodhanhassan99 <ajfreeforall@gmail.com>
2026-06-14 20:32:37 +0300

    corrections

--- _config.js
+++ _config.js
@@ -1,11 +1,7 @@
-var config = {};
-
-const dbPassword = process.env.DB_PASSWORD; 
-
-config.mongoURI = {
-    production: `mongodb+srv://hodhan:${dbPassword}@cluster0.v9ky2b5.mongodb.net/darkroom?retryWrites=true&w=majority`,
-    development: `mongodb+srv://hodhan:${dbPassword}@cluster0.v9ky2b5.mongodb.net/darkroom-dev?retryWrites=true&w=majority`,
-    test: `mongodb+srv://hodhan:${dbPassword}@cluster0.v9ky2b5.mongodb.net/darkroom-test?retryWrites=true&w=majority`
-}
-
-module.exports = config;
+module.exports = {
+    mongoURI: {
+        production: `mongodb+srv://hodhan:${process.env.DB_PASSWORD}@cluster0.v9ky2b5.mongodb.net/darkroom?retryWrites=true&w=majority`,
+        development: `mongodb+srv://hodhan:${process.env.DB_PASSWORD}@cluster0.v9ky2b5.mongodb.net/darkroom-dev?retryWrites=true&w=majority`,
+        test: `mongodb+srv://hodhan:${process.env.DB_PASSWORD}@cluster0.v9ky2b5.mongodb.net/darkroom-test?retryWrites=true&w=majority`
+    }
+};
