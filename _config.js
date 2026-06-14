d135b94c49185c7f297e4d68985e708597eaffc7
hodhanhassan99 <ajfreeforall@gmail.com>
2026-06-14 18:51:14 +0300

    changed

--- _config.js
+++ _config.js
@@ -1,9 +1,11 @@
-var config = {}
+var config = {};
+
+const dbPassword = process.env.DB_PASSWORD; 
 
 config.mongoURI = {
-    production: 'mongodb+srv://hodhan:6161332200p@cluster0.v9ky2b5.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://hodhan:6161332200p@cluster0.v9ky2b5.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://hodhan:6161332200p@cluster0.v9ky2b5.mongodb.net/darkroom-test?retryWrites=true&w=majority'
+    production: `mongodb+srv://hodhan:${dbPassword}@cluster0.v9ky2b5.mongodb.net/darkroom?retryWrites=true&w=majority`,
+    development: `mongodb+srv://hodhan:${dbPassword}@cluster0.v9ky2b5.mongodb.net/darkroom-dev?retryWrites=true&w=majority`,
+    test: `mongodb+srv://hodhan:${dbPassword}@cluster0.v9ky2b5.mongodb.net/darkroom-test?retryWrites=true&w=majority`
 }
 
-module.exports =  config;
+module.exports = config;
