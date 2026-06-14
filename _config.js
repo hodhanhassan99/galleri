577fafcb3c66410cbbe569c676d964572925f2eb
hodhanhassan99 <ajfreeforall@gmail.com>
2026-06-14 20:48:13 +0300

    fix

--- _config.js
+++ _config.js
@@ -1,7 +1,7 @@
 module.exports = {
     mongoURI: {
-        production: `mongodb+srv://hodhan:${process.env.DB_PASSWORD}@cluster0.v9ky2b5.mongodb.net/darkroom?retryWrites=true&w=majority`,
-        development: `mongodb+srv://hodhan:${process.env.DB_PASSWORD}@cluster0.v9ky2b5.mongodb.net/darkroom-dev?retryWrites=true&w=majority`,
-        test: `mongodb+srv://hodhan:${process.env.DB_PASSWORD}@cluster0.v9ky2b5.mongodb.net/darkroom-test?retryWrites=true&w=majority`
+        production: `mongodb+srv://hodhan:${process.env.DB_PASSWORD}@cluster0.v9ky2b5.mongodb.net/darkroom?appName=Cluster0`,
+        development: `mongodb+srv://hodhan:${process.env.DB_PASSWORD}@cluster0.v9ky2b5.mongodb.net/darkroom-dev?appName=Cluster0`,
+        test: `mongodb+srv://hodhan:${process.env.DB_PASSWORD}@cluster0.v9ky2b5.mongodb.net/darkroom-test?appName=Cluster0`
     }
 };
